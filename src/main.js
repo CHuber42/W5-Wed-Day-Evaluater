814ed9cdb5f0e9b581b373c2c15d93646baaf596
Christopher Huber <CHuber42.work@gmail.com>
2020-04-15 13:49:50 -0700

    add switch to convert day index in date object to plain name

--- src/main.js
+++ src/main.js
@@ -42,7 +42,25 @@ $("button").click(function() {
   let month = parseInt($("#inputMonth").val());
   if (validateInputs(year, month, day) == "Valid") {
     let inputDate = new Date(year, month - 1, day);
-    console.log(inputDate)
+    let conclusionDay = inputDate.getDay();
+    let dayOfWeek = "";
+    switch (conclusionDay) {
+      case 0: dayOfWeek = "Sunday";
+        break;
+      case 1: dayOfWeek = "Monday";
+        break;
+      case 2: dayOfWeek = "Tuesday";
+        break;
+      case 3: dayOfWeek = "Wednesday";
+        break;
+      case 4: dayOfWeek = "Thursday";
+        break;
+      case 5: dayOfWeek = "Friday";
+        break;
+      case 6: dayOfWeek = "Saturday";
+        break;
+    }
+    
   }
   else {
     //terminate program with error message
