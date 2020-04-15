6c86038b5c0240b24a622470d1c8c5e4fdd88072
Christopher Huber <CHuber42.work@gmail.com>
2020-04-15 14:01:57 -0700

    moving functions to helper files

--- src/main.js
+++ src/main.js
@@ -1,40 +1,9 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './styles.css';
+import './helper-functions'
 
-export function validateInputs(year, month, day) {
-  let highestDay = 0; 
-  switch (month) {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-      highestDay = 31;
-      break;
-    case 2: 
-      highestDay = 28;
-      break;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-      highestDay = 30;
-      break;
-    default: 
-      return "Invalid";
-  }
-  
-  if ((day < 1) || (day > highestDay))  {
-    return "Invalid";
-  }
-  if ((year < -271820) || (year > 275759)) {
-    return "Invalid";
-  } 
-  return "Valid";
-}
+
 
 $("button").click(function() {
   $("#outputDay").empty();
