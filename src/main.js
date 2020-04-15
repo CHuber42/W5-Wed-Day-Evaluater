47d95778925e2e53af69f1a5217035e41ff1b919
Christopher Huber <CHuber42.work@gmail.com>
2020-04-15 11:09:04 -0700

    add js for validating month/day inputs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,40 @@
 import 'bootstrap';
 import './styles.css';
 
-let now = new Date();
-let today = now.getDay();
-console.log(today)
+function validateInputs(year, month, day) {
+  let highestDay = 0; 
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      highestDay = 31;
+      break;
+    case 2: 
+      highestDay = 28;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      highestDay = 30;
+      break;
+    default: 
+      return "Invalid";
+  }
+  
+  if ((day < 1) || (day > highestDay))  {
+    return "Invalid";
+  }
+}
+
+$("button").click(function() {
+  let day = parseInt($("#inputDate").val());
+  let year = parseInt($("#inputYear").val());
+  let month = parseInt($("#inputMonth").val());
+  validateInputs(year, month, day);
+  new Date(year, month, day);
+});
